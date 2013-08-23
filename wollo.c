a04f102610f4e06711d8bfb29b2a9c7d5ea6007f
Breno <brenolimadefreitas@gmail.com>
2013-08-23 19:08:12 -0300

    gitignore

--- wollo.c
+++ wollo.c
@@ -1,8 +1,16 @@
 #include "wollo.h"
 
+static char nextToken(){
+	return 'x';
+}
+
 void print(const char *format, ...){
 	va_list args;
 	va_start(args, format);
 	vprintf(format, args);
-	va_end (args);
+	va_end(args);
+
+	printf("%c\n", nextToken());
 }
+
+#define nextToken NULL
